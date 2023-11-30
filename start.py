d100d662085d82a303da6226bede4bad8a628bae
Alok kumar Prajapti <alokprajapati913@gmail.com>
2023-11-30 10:49:08 +0530

    [Alok]Added:code start with display welcom to Employee

---
+++ start.py
@@ -0,0 +1,15 @@
+"""
+
+@Author: Alok kumar
+
+@Date: 29-11-23 2:43:30
+
+@Last Modified by: Alok kumar
+
+@Last Modified time: 24-11-2023 4:00:30
+
+@Title : Implementation of Employee Wage computation problem
+
+"""
+
+print("Start with display welcome to Employee wage computation program")
