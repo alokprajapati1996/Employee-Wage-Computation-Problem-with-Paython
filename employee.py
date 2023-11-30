7dfb88686ac9b1bf31e0a0acb904efe488e92a82
Alok kumar Prajapti <alokprajapati913@gmail.com>
2023-11-30 17:17:51 +0530

    [Added]Add: cade calculate employee daily wage

--- employee.py
+++ employee.py
@@ -2,28 +2,41 @@
 
 @Author: Alok kumar
 
-@Date: 29-11-23 2:43:30
+@Date: 30-11-23 10:38:30
 
 @Last Modified by: Alok kumar
 
-@Last Modified time: 24-11-2023 4:00:30
+@Last Modified time: 30-11-2023 4:11:30
 
 @Title : Implementation of Employee Wage computation problem
 
 """
 import random
 
-print("welcome to Employee wage computation program")
+
+def daily_emp_wage():
+    """
+    @parameter : None
+
+    :return : None
+
+    """
+
+    print("welcome to Employee wage computation")
 
 
-def check_attandance():
-    FULL_TIME = 1
-    emp_type = random.randint(0, 1)
-    if emp_type == FULL_TIME:
-        print("Employee is Present")
-    else:
-        print("Employee is Absent")
+FULL_TIME = 1
+WAGE_PER_HOUR = 20
+emp_type = (random.randint(0, 1))
+workings_hours = 0
+if emp_type == FULL_TIME:
 
+    print(" Employee is present.")
+    workings_hours = 8
+else:
+    print(" Employee is Absent.")
+wage = workings_hours * WAGE_PER_HOUR
+print("Employee daily wage is : ", wage)
 
 if __name__ == '__main__':
-    check_attandance()
+    daily_emp_wage()
