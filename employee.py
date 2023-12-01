3614ea93e58aa458699eff8870223e5b6673111c
Alok kumar Prajapti <alokprajapati913@gmail.com>
2023-12-02 04:55:43 +0530

    [Added]Add:code calculate wage max hour hundred and day twenty

--- employee.py
+++ employee.py
@@ -27,25 +27,29 @@ def daily_emp_wage():
 
     # FULL_TIME = 1
     # PART_TIME = 2
-    WAGE_PER_HOUR = 20
-    NUM_OF_WORKING_DAYS = 20
+    wage_per_hour = 20
     total_emp_wage = 0
     workings_hours = 0
-    for day in range(0, NUM_OF_WORKING_DAYS):
+    max_working_days = 20
+    max_working_hours = 100
+    total_emp_hour = 0
+    total_work_day = 0
+    while total_emp_hour <= max_working_hours and total_work_day < max_working_days:
         emp_type = (random.randint(0, 2))
+        total_work_day = total_work_day + 1
         match emp_type:
             case 0:
-                print(" Employee is Absent.", day)
+                print(" Employee is Absent .")
                 workings_hours = 0
             case 1:
-                print(" Employee is working full time")
+                print(" Employee is working full time .")
                 workings_hours = 8
             case 2:
                 print(" Employee is working part time.")
                 workings_hours = 4
-        emp_wage = workings_hours * WAGE_PER_HOUR
-        total_emp_wage = total_emp_wage + emp_wage
-    print("Employee daily wage is : ", total_emp_wage)
+        total_emp_hour += workings_hours
+        total_emp_wage = total_emp_hour * wage_per_hour
+    print("One month total Employee wage : ", total_emp_wage)
 
 
 if __name__ == '__main__':
