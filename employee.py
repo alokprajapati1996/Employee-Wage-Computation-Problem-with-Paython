5fe337cfe5a5be639dbebedd6e3733cd706a3279
Alok kumar Prajapti <alokprajapati913@gmail.com>
2023-12-01 06:34:39 +0530

    [Added]Add:use switch case calculate part and full time daily wage

--- employee.py
+++ employee.py
@@ -15,7 +15,6 @@ import random
 
 
 def daily_emp_wage():
-
     """
 
  @parameter : None
@@ -25,19 +24,23 @@ def daily_emp_wage():
     """
 
     print("welcome to Employee wage computation")
+
+
 FULL_TIME = 1
 PART_TIME = 2
 WAGE_PER_HOUR = 20
 emp_type = (random.randint(0, 2))
 workings_hours = 0
-if emp_type == FULL_TIME:
-    print(" Employee is working full time")
-    workings_hours = 8
-elif emp_type == PART_TIME:
-    print(" Employee is working part time.")
-    workings_hours = 4
-else:
-    print(" Employee is Absent.")
+match emp_type:
+    case 1:
+        print(" Employee is working full time")
+        workings_hours = 8
+
+    case 2:
+        print(" Employee is working part time.")
+        workings_hours = 4
+    case 0:
+        print(" Employee is Absent.")
 emp_wage = workings_hours * WAGE_PER_HOUR
 print("Employee daily wage is : ", emp_wage)
 
