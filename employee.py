c5b4153333c629544735927dfd18db009dcaa108
Alok kumar Prajapti <alokprajapati913@gmail.com>
2023-12-07 18:11:21 +0530

    [Alok] Add:refactor the code to use class and method to compute emp wage

--- employee.py
+++ employee.py
@@ -14,43 +14,54 @@
 import random
 
 
-def daily_emp_wage():
-    """
+class Employee:
 
- @parameter : None
+    def __init__(self, emp_name, workings_hours, max_working_days, max_working_hours):
+        self.emp_name = emp_name
+        self.workings_hours = workings_hours
+        self.max_working_days = max_working_days
+        self.max_working_hours = max_working_hours
 
-    :return : None
+    def daily_emp_wage(self):
+        """
+         Description : 
+         parameter : None
+         return : None
 
-    """
+        """
 
-    print("welcome to Employee wage computation")
+        print("welcome to Employee wage computation")
 
-    # FULL_TIME = 1
-    # PART_TIME = 2
-    wage_per_hour = 20
-    total_emp_wage = 0
-    workings_hours = 0
-    max_working_days = 20
-    max_working_hours = 100
-    total_emp_hour = 0
-    total_work_day = 0
-    while total_emp_hour <= max_working_hours and total_work_day < max_working_days:
-        emp_type = (random.randint(0, 2))
-        total_work_day = total_work_day + 1
-        match emp_type:
-            case 0:
-                print(" Employee is Absent .")
-                workings_hours = 0
-            case 1:
-                print(" Employee is working full time .")
-                workings_hours = 8
-            case 2:
-                print(" Employee is working part time.")
-                workings_hours = 4
-        total_emp_hour += workings_hours
-        total_emp_wage = total_emp_hour * wage_per_hour
-    print("One month total Employee wage : ", total_emp_wage)
+        total_emp_wage = 0
+        total_emp_hour = 0
+        total_work_day = 0
+        while total_emp_hour <= self.max_working_hours and total_work_day < self.max_working_days:
+            emp_type = (random.randint(0, 2))
+            total_work_day = total_work_day + 1
+            match emp_type:
+                case 0:
+                    print(" Employee is Absent .")
+                    self.workings_hours = 0
+                case 1:
+                    print(" Employee is working full time .")
+                    self.workings_hours = 8
+                case 2:
+                    print(" Employee is working part time.")
+                    self.workings_hours = 4
+            total_emp_hour += self.workings_hours
+            total_emp_wage = total_emp_hour * wage_per_hour
+        print(f"{self.emp_name} One month total Employee wage : ", total_emp_wage)
+
+
+def main():
+    emp_name = input("Enter Employee name : ")
+    workings_hours = int(input("enter working hours: "))
+    max_working_days = int(input("enter working days: "))
+    max_working_hours = int(input("enter max working hours: "))
+
+    emp_obj = Employee(emp_name, workings_hours, max_working_days, max_working_hours)
+    emp_obj.daily_emp_wage()
 
 
 if __name__ == '__main__':
-    daily_emp_wage()
+    main()
